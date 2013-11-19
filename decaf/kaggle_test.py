71616faa58e515f48d29566ff6b5960ec597cc79
Kyle Kastner <kastnerkyle@gmail.com>
2013-11-18 19:14:09 -0600

    Optimized SVM provided near 0 improvement... back to the drawing board

--- decaf/kaggle_test.py
+++ decaf/kaggle_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import pickle
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 clf = pickle.load(open('saved_clf.pkl', 'rb'))
 tst = pickle.load(open('saved_tst.pkl', 'rb'))
+print clf.get_params()
 res = clf.predict(tst)
 out = np.vstack((np.array(range(1, res.shape[0] + 1)), res)).astype('int').T
 np.savetxt('results.csv', out, header='id,label', fmt='%i,%i', delimiter=',')
--- decaf/kaggle_train.py
+++ decaf/kaggle_train.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import pickle
+from sklearn.svm import SVC
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import RandomizedSearchCV
 from sklearn.metrics import accuracy_score
 
 x = pickle.load(open('saved_x.pkl', 'rb'))
@@ -10,7 +11,25 @@ y = pickle.load(open('saved_y.pkl', 'rb'))
 X_train, X_test, y_train, y_test = train_test_split(x, y,
                                                     test_size=.2,
                                                     random_state=42)
-clf = LogisticRegression()
+n_iter_search = 50
+param_dist = {'degree': [1, 2, 3, 4, 5],
+              'C': [np.random.randint(1, 4) * np.random.random_sample()
+                    for i in range(20)],
+              'shrinking': [True, False]}
+#Last chosen params were
+#{'kernel': 'poly', 'C': 0.6355611416796046, 'verbose': False,
+#'probability': False, 'degree': 2, 'shrinking': True, 'max_iter': -1,
+#'random_state': None, 'tol': 0.001, 'cache_size': 200, 'coef0': 0.0,
+#'gamma': 0.0, 'class_weight': None}
+
+random_search = RandomizedSearchCV(SVC(kernel='poly'),
+                                   param_dist,
+                                   n_iter=n_iter_search,
+                                   refit=True)
+random_search.fit(X_train, y_train)
+clf = random_search.best_estimator_
+
+
 def score(clf, X_train, y_train, X_test, y_test):
     clf.fit(X_train, y_train)
     q = clf.predict(X_test)
